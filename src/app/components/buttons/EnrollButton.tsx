a068d3d756a93f432e264735a4ca19a62d60659e
kervengully <gullyyevv@gmail.com>
2024-07-17 20:35:40 +0300

    Update EnrollButton.tsx

--- src/app/components/buttons/EnrollButton.tsx
+++ src/app/components/buttons/EnrollButton.tsx
@@ -8,7 +8,7 @@ const EnrollButton: React.FC<EnrollButtonProps> = ({ }) => {
   const router = useRouter();
   return (
     <button onClick={() => router.push('https://iws-register.iwsonlineschool.co.uk/')} className="bg-blue-600 rounded-lg px-6 py-2 text-white hover:bg-blue-700 transition-all duration-300 font-semibold text-lg">
-      Enroll Now
+      Enrol Now
     </button>
   )
 }
