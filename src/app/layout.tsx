c8e1d5487b2c139ff79a5c9c2aef3d805f3e31bd
Kerim-Gullyyew <gullyyevk@gmail.com>
2024-07-23 11:00:12 +0300

    t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,13 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import type { Viewport } from "next";
-import "@fontsource/sora";
-import "@fontsource/inter";
+import { Sora } from 'next/font/google';
 
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['400', '700', '800'],
+})
 
 export const viewport: Viewport = {
   colorScheme: "normal",
@@ -31,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={sora.className}>
         <header>
           <Header />
         </header>
