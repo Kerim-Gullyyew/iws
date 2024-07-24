fae401715cb5e8c5ff19372f90a76121425617ed
Kerim-Gullyyew <gullyyevk@gmail.com>
2024-07-24 16:36:33 +0300

    t

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -7,11 +7,11 @@ interface pageProps {
 const page: React.FC<pageProps> = ({ }) => {
   return (
     <div className='container'>
-      <div className='flex justify-center items-center py-14 lg:py-24'>
-        <TabGroup>
-          <TabList className={"border-2 p-3"}>
-            <Tab>Send a message</Tab>
-            <Tab>Book a Call</Tab>
+      <div className='flex w-full justify-center items-center py-14 lg:py-24'>
+        <TabGroup className={''}>
+          <TabList className={"border-2 px-4 py-2 rounded-xl flex items-center justify-center gap-5"}>
+            <Tab className={'bg-blue-600 px-6 py-3 rounded-xl text-white outline-none'}>Send a message</Tab>
+            <Tab className={'py-3 px-6 rounded-xl text-blue-900 outline-none'}>Book a Call</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>Content 1</TabPanel>
