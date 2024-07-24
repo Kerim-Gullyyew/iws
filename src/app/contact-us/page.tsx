b17ffbadd5aa8b86b6725c90a1a8c75f965d5622
Kerim-Gullyyew <gullyyevk@gmail.com>
2024-07-24 19:31:22 +0300

    t

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,26 +1,33 @@
+"use client"
 import React from 'react'
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import TabSection from '../components/ui/TabSection'
+import ContactUsSection from '../components/sections/ContactUsSection'
+import { InlineWidget } from "react-calendly";
 interface pageProps {
 
 }
 
 const page: React.FC<pageProps> = ({ }) => {
   return (
-    <div className='container'>
-      <div className='flex w-full justify-center items-center py-14 lg:py-24'>
-        <TabGroup className={''}>
-          <TabList className={"border-2 px-4 py-2 rounded-xl flex items-center justify-center gap-5"}>
-            <Tab className={'bg-blue-600 px-6 py-3 rounded-xl text-white outline-none'}>Send a message</Tab>
-            <Tab className={'py-3 px-6 rounded-xl text-blue-900 outline-none'}>Book a Call</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>Content 1</TabPanel>
-            <TabPanel>Content 2</TabPanel>
-            <TabPanel>Content 3</TabPanel>
-          </TabPanels>
-        </TabGroup>
+    <div className='container py-14 lg:py-24'>
 
-      </div>
+      <TabGroup defaultIndex={1} className={'w-full flex flex-col justify-center items-center gap-10'}>
+        <TabList className={"w-full border-2 px-2 py-2 rounded-xl flex  justify-center gap-5 max-w-[500px]"}>
+          <Tab className={'data-[selected]:bg-blue-800 transition duration-300 data-[selected]:animate-fade-in bg-white flex-1 px-3 py-3 rounded-xl text-black data-[selected]:text-white outline-none'}>Send a message</Tab>
+          <Tab className={'data-[selected]:bg-blue-800 transition duration-300 data-[selected]:animate-fade-in bg-white flex-1 px-3 py-3 rounded-xl text-black data-[selected]:text-white outline-none'}>Book a Call</Tab>
+        </TabList>
+        <TabPanels className={'w-full'}>
+          <TabPanel>
+            <ContactUsSection />
+          </TabPanel>
+          <TabPanel>
+
+            <InlineWidget styles={{ height: '1000px' }} url="https://calendly.com/admissions-iws/book-a-call" />
+
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
 
     </div>
   )
