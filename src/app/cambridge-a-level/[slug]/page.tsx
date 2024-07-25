af3a3eca2e9976622bbbd3aa4b6b603f3c3b333f
Kerim-Gullyyew <gullyyevk@gmail.com>
2024-07-25 14:05:15 +0300

    t

--- src/app/cambridge-a-level/[slug]/page.tsx
+++ src/app/cambridge-a-level/[slug]/page.tsx
@@ -2,13 +2,16 @@
 import React from "react";
 import { courses } from "@/app/data/alevel";
 import CoursePage from "@/app/components/ui/CoursePage";
+import type { Metadata, ResolvingMetadata } from 'next'
+
+
 interface pageProps {
-  params: {
-    slug: string;
-  };
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-const page: React.FC<pageProps> = ({ params }: { params: { slug: string } }) => {
+
+const page: React.FC<pageProps> = ({ params, searchParams }) => {
   const course = courses.find((course) => course.url === params.slug);
 
   return <CoursePage course={course} />
