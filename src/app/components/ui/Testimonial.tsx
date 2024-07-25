f930a9c3d16d716b88f8a48562e2c95efbaf6048
kervengully <gullyyevv@gmail.com>
2024-07-25 19:53:41 +0300

    1

--- src/app/components/ui/Testimonial.tsx
+++ src/app/components/ui/Testimonial.tsx
@@ -18,40 +18,40 @@ const Testimonial: React.FC<TestimonialProps> = ({ }) => {
     <>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         <SwiperSlide>
-          <div className='container max-w-4xl flex flex-col gap-14'>
-            <p className='text-center text-p text-blue-900 leading-10'>“Enrolling my child in IWS Online School was the best decision we made. The flexible schedule and personalised attention helped my child excel academically. The curriculum is top-notch, and the instructors are exceptional. Thank you, IWS!” </p>
+          <div className='px-2 sm:px-0 sm:container max-w-4xl flex flex-col gap-7 sm:gap-14'>
+            <p className='text-center text-p text-blue-900 leading-8 sm:leading-10'>“Enrolling my child in IWS Online School was the best decision we made. The flexible schedule and personalised attention helped my child excel academically. The curriculum is top-notch, and the instructors are exceptional. Thank you, IWS!” </p>
 
             <h4 className='  text-blue-600 text-center'>Sarah Thompson (Parent)</h4>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className='container max-w-4xl flex flex-col gap-14'>
-            <p className='text-center text-p text-blue-900 leading-10'>“IWS Online School provided me with a quality education that laid a strong foundation for my future. The interactive learning experience and 24/7 access to lessons were invaluable. I highly recommend IWS to any student seeking a flexible and enriching academic journey.”</p>
+          <div className='px-2 sm:px-0 sm:container max-w-4xl flex flex-col gap-7 sm:gap-14'>
+            <p className='text-center text-p text-blue-900 leading-8 sm:leading-10'>“IWS Online School provided me with a quality education that laid a strong foundation for my future. The interactive learning experience and 24/7 access to lessons were invaluable. I highly recommend IWS to any student seeking a flexible and enriching academic journey.”</p>
 
             <h4 className='  text-blue-600 text-center'>Charlotte Rodriguez (IGCSE)</h4>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className='container max-w-4xl flex flex-col gap-14'>
-            <p className='text-center text-p text-blue-900 leading-10'>“Being a part of IWS Online School has been a fantastic journey. The virtual classrooms provide a dynamic learning environment, and the comprehensive resources. The teachers are amazing - they make complex subjects easy to understand and are invested in our success.”</p>
+          <div className='px-2 sm:px-0 sm:container max-w-4xl flex flex-col gap-7 sm:gap-14'>
+            <p className='text-center text-p text-blue-900 leading-8 sm:leading-10'>“Being a part of IWS Online School has been a fantastic journey. The virtual classrooms provide a dynamic learning environment, and the comprehensive resources. The teachers are amazing - they make complex subjects easy to understand and are invested in our success.”</p>
 
             <h4 className='  text-blue-600 text-center'>Eleanor Smith (A Level)</h4>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className='container max-w-4xl flex flex-col gap-14'>
-            <p className='text-center text-p text-blue-900 leading-10'>“IWS Online School has been a revelation for me. The interactive virtual classes and well-structured curriculum have made learning a truly enriching experience. The teachers are not just knowledgeable but also approachable, always ready to help. Thank you, IWS!”</p>
+          <div className='px-2 sm:px-0 sm:container max-w-4xl flex flex-col gap-7 sm:gap-14'>
+            <p className='text-center text-p text-blue-900 leading-8 sm:leading-10'>“IWS Online School has been a revelation for me. The interactive virtual classes and well-structured curriculum have made learning a truly enriching experience. The teachers are not just knowledgeable but also approachable, always ready to help. Thank you, IWS!”</p>
 
             <h4 className='  text-blue-600 text-center'>Jinsha Peter (A Level)</h4>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className='container max-w-4xl flex flex-col gap-14'>
-            <p className='text-center text-p text-blue-900 leading-10'>{"“As a parent, I am thoroughly impressed with IWS Online School. The dedication of the instructors and the emphasis on goal-driven education have had a profound impact on my child's academic growth. The supportive community fostered by IWS is truly Commendable.”"}</p>
+          <div className='px-2 sm:px-0 sm:container max-w-4xl flex flex-col gap-7 sm:gap-14'>
+            <p className='text-center text-p text-blue-900 leading-8 sm:leading-10'>{"“As a parent, I am thoroughly impressed with IWS Online School. The dedication of the instructors and the emphasis on goal-driven education have had a profound impact on my child's academic growth. The supportive community fostered by IWS is truly Commendable.”"}</p>
 
             <h4 className='  text-blue-600 text-center'>Emily Turner (Parent)</h4>
           </div>
