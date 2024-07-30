51cfe3bd57736e0b98c0a75d6d13d391c2c82cf5
kervengully <gullyyevv@gmail.com>
2024-07-30 12:20:59 +0300

    Update layout.tsx

--- src/app/cambridge-a-level/layout.tsx
+++ src/app/cambridge-a-level/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "IWS Online School - A Level",
+  title: "IWS Online A Level Courses | Cambridge Online A Levels Distance Learning",
   description: "Cambridge Accredited Online A Level Courses for flexible distance learning. Study your chosen Online A Level subjects at your own pace with unlimited support.",
   abstract: "Cambridge Accredited Online A Level Courses for flexible distance learning. Study your chosen Online A Level subjects at your own pace with unlimited support.",
 };
