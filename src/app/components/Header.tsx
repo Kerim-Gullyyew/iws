1bc7cae9467dac0542bb3d436324e4e83f4d3633
kervengully <gullyyevv@gmail.com>
2024-10-21 16:20:27 +0300

    1

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,48 +1,30 @@
-"use client";
-import { useState, useEffect } from "react";
-import { Dialog, Popover } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import Image from "next/image";
-import EnrollButton from "./buttons/EnrollButton";
-import { usePathname } from "next/navigation";
+"use client"
+import { useState } from 'react'
+import { Dialog, Popover } from '@headlessui/react'
+import {
+  Bars3Icon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import Image from 'next/image'
+import EnrollButton from './buttons/EnrollButton'
+import { usePathname } from 'next/navigation';
+interface HeaderProps {
 
-interface HeaderProps {}
+}
 
-const Header: React.FC<HeaderProps> = ({}) => {
-  const pathname = usePathname();
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [initialUrl, setInitialUrl] = useState<string | null>(null);
+const Header: React.FC<HeaderProps> = ({ }) => {
+  const pathname = usePathname()
 
-  useEffect(() => {
-    const storedUrl = localStorage.getItem("initialUrl");
-    if (storedUrl) {
-      const modifiedUrl = storedUrl.replace("https://iwsonlineschool.co.uk", "");
-      setInitialUrl(modifiedUrl);
-    }
-  }, []);
-
-  const handleApplyClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    if (initialUrl) {
-      e.preventDefault();
-      window.location.href = `https://iws-register.iwsonlineschool.co.uk/?initialUrl=${encodeURIComponent(initialUrl)}`;
-    }
-  };
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <header className="fixed w-full isolate z-50 bg-white shadow-sm">
       <nav className="mx-auto container flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <Link className="-m-1.5 p-1.5" href={"/"}>
+          <Link className="-m-1.5 p-1.5" href={"/"} >
             <span className="sr-only">IWS Online School</span>
-            <Image
-              priority={true}
-              width={1000}
-              height={1000}
-              className="h-[3rem] w-auto"
-              src="/IWS_Logo.webp"
-              alt="IWS Online School Icon"
-            />
+            <Image priority={true} width={1000} height={1000} className="h-[3rem] w-auto" src="/IWS_Logo.webp" alt="IWS Online School Icon" />
           </Link>
         </div>
         <div className="flex lg:hidden sm:items-center sm:gap-4">
@@ -59,30 +41,35 @@ const Header: React.FC<HeaderProps> = ({}) => {
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-1">
-          <>
-            <Link className="leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px]" href={"/cambridge-primary/"}>
-              Primary
-            </Link>
-            <Link className="leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px]" href={"/cambridge-secondary/"}>
-              Secondary
-            </Link>
-            <Link className="leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px]" href={"/cambridge-igcse/"}>
-              I/GCSE
-            </Link>
-            <Link className="leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px]" href={"/cambridge-a-level/"}>
-              A Level
-            </Link>
-            <Link className="leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px]" href={"/school-fees/"}>
-              School Fees
-            </Link>
-            <Link className="leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px]" href={"/contact-us/"}>
-              Contact Us
-            </Link>
-          </>
+          {
+            <>
+              <Link className=' leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px] ' href={"/cambridge-primary/"} >
+                Primary
+              </Link>
+              <Link className=' leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px] ' href={"/cambridge-secondary/"} >
+                Secondary
+              </Link>
+              <Link className=' leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px] ' href={"/cambridge-igcse/"} >
+                I/GCSE
+              </Link>
+              <Link className=' leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px] ' href={"/cambridge-a-level/"} >
+                A Level
+              </Link>
+              <Link className=' leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px] ' href={"/school-fees/"} >
+                School Fees
+              </Link>
+              <Link className=' leading-6 hover:bg-[#E6F4FE] px-6 py-2 rounded-lg transition duration-300 text-[18px] ' href={"/contact-us/"} >
+                Contact Us
+              </Link>
+            </>
+
+          }
+
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <EnrollButton />
         </div>
+
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
@@ -90,13 +77,16 @@ const Header: React.FC<HeaderProps> = ({}) => {
           <div className="flex items-center justify-between">
             <Link onClick={() => setMobileMenuOpen(false)} href={"/"} className="-m-1.5 p-1.5">
               <span className="sr-only">IWS Online School</span>
-              <Image width={1000} height={1000} className="h-8 w-auto" src="/IWS_Logo.webp" alt="IWS Online School Icon" />
+              <Image width={1000} height={1000}
+                className="h-8 w-auto"
+                src="/IWS_Logo.webp" alt="IWS Online School Icon"
+              />
             </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(false)}
-            >
+            >a
               <span className="sr-only">Close menu</span>
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
@@ -104,36 +94,60 @@ const Header: React.FC<HeaderProps> = ({}) => {
           <div className="pt-[60px] flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Link onClick={() => setMobileMenuOpen(false)} href={"/cambridge-primary/"} className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-gray-50">
-                  Primary
-                </Link>
-                <Link onClick={() => setMobileMenuOpen(false)} href={"/cambridge-secondary/"} className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-gray-50">
+
+                <Link onClick={() => setMobileMenuOpen(false)} href={"/cambridge-primary/"} className="-mx-3 block rounded-lg px-3 py-2 text-base  leading-7  hover:bg-gray-50"> Primary</Link>
+                <Link
+                  onClick={() => setMobileMenuOpen(false)}
+                  href={"/cambridge-secondary/"}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base  leading-7  hover:bg-gray-50"
+                >
                   Secondary
                 </Link>
-                <Link onClick={() => setMobileMenuOpen(false)} href={"/cambridge-igcse/"} className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-gray-50">
+                <Link
+                  onClick={() => setMobileMenuOpen(false)}
+                  href={"/cambridge-igcse/"}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base  leading-7  hover:bg-gray-50"
+                >
                   I/GCSE
                 </Link>
-                <Link onClick={() => setMobileMenuOpen(false)} href={"/cambridge-a-level/"} className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-gray-50">
+                <Link
+                  onClick={() => setMobileMenuOpen(false)}
+                  href={"/cambridge-a-level/"}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base  leading-7  hover:bg-gray-50"
+                >
                   A Level
                 </Link>
+
               </div>
               <div className="py-6">
-                <Link onClick={() => setMobileMenuOpen(false)} href={"/school-fees/"} className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-gray-50">
+                <Link
+                  onClick={() => setMobileMenuOpen(false)}
+                  href={"/school-fees/"}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base  leading-7  hover:bg-gray-50"
+                >
                   School Fees
                 </Link>
-                <Link onClick={() => setMobileMenuOpen(false)} href={"/about-us/"} className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-gray-50">
+                <Link
+                  onClick={() => setMobileMenuOpen(false)}
+                  href={"/about-us/"}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base  leading-7  hover:bg-gray-50"
+                >
                   About us
                 </Link>
-                <Link href="https://iws-register.iwsonlineschool.co.uk/" onClick={handleApplyClick} className="-mx-3 block rounded-lg px-3 py-2.5 text-base leading-7 hover:bg-gray-50">
+                {/* <Link
+                  href={"https://iws-register.iwsonlineschool.co.uk/"}
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base  leading-7  hover:bg-gray-50"
+                >
                   Apply Now
-                </Link>
+                </Link> */}
+                <EnrollButton />
               </div>
             </div>
           </div>
         </Dialog.Panel>
       </Dialog>
-    </header>
-  );
-};
+    </header >
+  )
+}
 
-export default Header;
+export default Header
--- src/app/components/buttons/EnrollButton.tsx
+++ src/app/components/buttons/EnrollButton.tsx
@@ -1,18 +1,46 @@
-"use client"
-import React from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
 interface EnrollButtonProps {
-  light?: boolean
+  light?: boolean;
 }
 
 const EnrollButton: React.FC<EnrollButtonProps> = ({ light }) => {
   const router = useRouter();
+
+  const handleClick = () => {
+    let newUrl = 'https://iws-register.iwsonlineschool.co.uk';
+
+    if (typeof window !== 'undefined') {
+      const initialUrl = localStorage.getItem('initialUrl');
+      if (initialUrl) {
+        try {
+          const url = new URL(initialUrl);
+          // Extract the path and query parameters
+          const pathAndQuery = url.pathname + url.search;
+          newUrl += pathAndQuery;
+        } catch (error) {
+          console.error('Invalid URL in localStorage:', initialUrl);
+        }
+      }
+    }
+
+    router.push(newUrl);
+  };
+
   return (
-    <button onClick={() => router.push('https://iws-register.iwsonlineschool.co.uk/')} className={`${light ? "bg-white rounded-full px-9 py-3 text-blue-600 font-semibold text-[18px]" : "bg-[#4366F6] rounded-xl px-7 py-3 text-white hover:bg-[#2C50C7] transition-all duration-300  text-lg"}`}>
+    <button
+      onClick={handleClick}
+      className={`${
+        light
+          ? 'bg-white rounded-full px-9 py-3 text-blue-600 font-semibold text-[18px]'
+          : 'bg-[#4366F6] rounded-xl px-7 py-3 text-white hover:bg-[#2C50C7] transition-all duration-300 text-lg'
+      }`}
+    >
       Apply now
     </button>
+  );
+};
 
-  )
-}
-
-export default EnrollButton
+export default EnrollButton;
