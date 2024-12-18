ab9d6243e59db5cdc6168d1fcd63f354fc57390f
kervengully <gullyyevv@gmail.com>
2024-12-18 14:31:56 +0200

    Update page.tsx

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,19 +1,26 @@
+"use client"
+import { useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Custom404() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = "/";
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <div className="flex flex-col gap-7 justify-center items-center text-center container h-[80vh]">
-        {/* <Image width={800} height={800} className=" " src="/404.webm" alt="" /> */}
-        <video width="640" height="360" loop>
+        <video width="640" height="360" loop autoPlay muted>
           <source src="/404.webm" type="video/webm" />
-          {/* <source src="/videos/sample.mp4" type="video/mp4" /> */}
           Your browser does not support the video tag.
         </video>
         <h1>Ooops! Page Not Found</h1>
         <p>
-          {"This page doesn't exist or remove! We suggest you back to home"}
+          {"This page doesn't exist or was removed! Redirecting you to the home page..."}
         </p>
         <Link
           className="px-8 py-3 bg-blue-600 text-white rounded-full w-fit"
